44429f385a58dd7ff3d02255499eeb8d014ad1e0
shiiya <c1508875@st.kanazawa-it.ac.jp>
2026-05-09 21:20:23 +0900

    プレフライト・リクエスト対応

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,6 +22,16 @@ export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
 
+    if (request.method === 'OPTIONS') {
+      return new Response(null, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type',
+        }
+      });
+    }
+
     // リクエストされたURLが「/api/auth」だった場合、user.jsの処理を呼び出す
     if (url.pathname === '/api/auth' && request.method === 'GET') {
       return handleUserAuth(request, env);
