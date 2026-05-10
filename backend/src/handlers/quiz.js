d427354a41b502d778af83d7c4b23d75dca56cc2
shiiya <c1508875@st.kanazawa-it.ac.jp>
2026-05-10 21:04:46 +0900

    ファイルの保存忘れ問題文の表示_2

--- backend/src/handlers/quiz.js
+++ backend/src/handlers/quiz.js
@@ -2,7 +2,7 @@
 export async function handleGetQuestions(request, env) {
   // SQLでデータをごっそり取ってくる
   const { results } = await env.DB.prepare(
-    'SELECT * FROM questions ORDER BY RANDOM() LIMIT 10' // ランダムに10問選ぶ
+    'SELECT * FROM questions ORDER BY RANDOM() LIMIT 15' // ランダムに15問選ぶ
   ).all();
 
   return new Response(JSON.stringify(results), {
